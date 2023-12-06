8b251deee1ca523c1a5512e737e15a57e1a66ce4
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-06 15:14:18 +0100

    feat: enable a few more key maps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,6 +11,9 @@ k.set("n", "<Tab>",   ":tabnext<cr>")
 k.set("n", "J", "mzJ`z")
 k.set("n", "<C-j>", "gea<cr><esc>")
 
+k.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
+k.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
+
 -- centralize the screen on scroll up/down or search next/previous
 k.set("n", "<C-d>", "<C-d>zz")
 k.set("n", "<C-u>", "<C-u>zz")
@@ -18,18 +21,20 @@ k.set("n", "n", "nzzzv")
 k.set("n", "N", "Nzzzv")
 
 -- Text manipulation
+k.set("n", "<leader>=", "gqip",   { desc = "Hard-wrap paragraphs of text" })
+
 k.set("x",        "<leader>p", [["_dP]], { desc = "Paste and keep the current buffer" })
 k.set({"n", "v"}, "<leader>d", [["_d]],  { desc = "Delete and keep the current buffer" })
-k.set("n",        "<leader>=", "gqip",   { desc = "Hard-wrap paragraphs of text" })
 
 -- show/hide hidden chars
 k.set("n", "<F12>", ":set invlist<cr>", { silent = true })
 
 -- search
 k.set("n", "<leader>/", ":nohlsearch<cr>", { desc = "Clears the search register" })
+k.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
+  desc = "Search and replace the currently selected text"
+})
 
 -- Buffers
 k.set("n", "<leader>bl", ":e#<cr>",      { desc = "Switch to last used buffer" })
 k.set("n", "<leader>bv", ":vsplit#<cr>", { desc = "Open last used buffer in vertical split" })
-
---k.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
