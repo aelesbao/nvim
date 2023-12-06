3bf28dc77687e39cb4caab7185fb2f0f37af2a89
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-06 17:52:51 +0100

    feat: add nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,6 +15,28 @@ return {
     opts = {},
   },
 
+  -- add/change/delete surrounding delimiter pairs with ease
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+      },
+    },
+  },
+
   -- notification pop-ups
   {
     "rcarriga/nvim-notify",
