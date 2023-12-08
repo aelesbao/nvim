c8f459ee41c4939ede0766e67c35a8bc85b68aeb
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-08 11:03:55 +0100

    feat: extra telescope grep mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,45 @@
+-- Telescope live_grep in git root
+-- Function to find the git root directory based on the current buffer's path
+local function find_git_root()
+  -- Use the current buffer's path as the starting point for the git search
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir
+  local cwd = vim.fn.getcwd()
+  -- If the buffer is not associated with a file, return nil
+  if current_file == '' then
+    current_dir = cwd
+  else
+    -- Extract the directory from the current file's path
+    current_dir = vim.fn.fnamemodify(current_file, ':h')
+  end
+
+  -- Find the Git root directory from the current file's path
+  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+  if vim.v.shell_error ~= 0 then
+    print 'Not a git repository. Searching on current working directory'
+    return cwd
+  end
+  return git_root
+end
+
+-- Custom live_grep function to search in git root
+local function telescope_live_grep_git_root()
+  local git_root = find_git_root()
+  if git_root then
+    require('telescope.builtin').live_grep {
+      search_dirs = { git_root },
+    }
+  end
+end
+
+local function telescope_live_grep_open_files()
+  require('telescope.builtin').live_grep {
+    grep_open_files = true,
+    prompt_title = 'Live Grep in Open Files',
+  }
+end
+
+
 return {
   -- Fuzzy finder for anything
   {
@@ -15,6 +57,8 @@ return {
 
       -- search
       { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>sG", telescope_live_grep_git_root,   desc = "Live grep on git root" },
+      { "<leader>s/", telescope_live_grep_open_files, desc = "Live grep on open files" },
       { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
       { "<leader>sm", "<cmd>Telescope marks<cr>",     desc = "Jump to mark" },
       { "<leader>sn", "<cmd>Telescope notify<cr>",    desc = "Notifications" },
