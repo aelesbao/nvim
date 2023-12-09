3a3b4701fadc433c8fdf339d06e0f273f004a2ad
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-09 12:58:53 +0100

    feat: extra tab keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,8 +4,10 @@ local k = vim.keymap
 k.set({'n', 'v'}, ";", ":", { remap = false })
 
 -- Tabs
-k.set("n", "<S-Tab>", ":tabprevious<cr>")
-k.set("n", "<Tab>",   ":tabnext<cr>")
+k.set("n", "<s-tab>", ":tabprevious<cr>")
+k.set("n", "<tab>",   ":tabnext<cr>")
+k.set("n", "<leader><tab>q", ":tabclose<cr>", { desc = "Close tab" })
+k.set("n", "<leader><tab>Q", ":tabonly<cr>",  { desc = "Close other tabs" })
 
 -- smart join/split lines
 k.set("n", "J", "mzJ`z")
@@ -21,10 +23,10 @@ k.set("n", "n", "nzzzv")
 k.set("n", "N", "Nzzzv")
 
 -- Text manipulation
-k.set("n", "<leader>=", "gqip",   { desc = "Hard-wrap paragraphs of text" })
+k.set("n", "<leader>=", "gqip", { desc = "Hard-wrap paragraphs of text" })
 
 k.set("x",        "<leader>p", [["_dP]], { desc = "Paste and keep the current buffer" })
-k.set({"n", "v"}, "<leader>d", [["_d]],  { desc = "Delete and keep the current buffer" })
+k.set({ "n", "v" }, "<leader>d", [["_d]],  { desc = "Delete and keep the current buffer" })
 
 -- show/hide hidden chars
 k.set("n", "<F12>", ":set invlist<cr>", { silent = true })
@@ -38,3 +40,6 @@ k.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
 -- Buffers
 k.set("n", "<leader>bl", ":e#<cr>",      { desc = "Switch to last used buffer" })
 k.set("n", "<leader>bv", ":vsplit#<cr>", { desc = "Open last used buffer in vertical split" })
+
+--map <leader>bw :call Wipeout()<cr>       -- delete all hidden buffers
+--nmap <silent> <LocalLeader>bd <Plug>Kwbd -- keep window on buffer delete
