595c46555c042a72b5153881326d5d2dc172d609
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-15 18:42:15 +0100

    feat: add GitHub copilot plugin

---
+++ lua/plugins/cmp/config.lua
@@ -0,0 +1,98 @@
+
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
+-- Here is where you configure the autocompletion settings.
+local lsp_zero = require("lsp-zero")
+lsp_zero.extend_cmp()
+
+-- And you can configure cmp even more, if you want to.
+-- local cmp_action = lsp_zero.cmp_action()
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+---@diagnostic disable-next-line: missing-fields
+cmp.setup({
+  formatting = lsp_zero.cmp_format(),
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  ---@diagnostic disable-next-line: missing-fields
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete({}),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() and has_words_before() then
+    --     cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    --   elseif luasnip.expand_or_locally_jumpable() then
+    --     luasnip.expand_or_jump()
+    --   else
+    --     fallback()
+    --   end
+    -- end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = {
+    { name = "buffer" },
+    { name = "crates" },
+    { name = "emoji" },
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "path" },
+    { name = "tmux" },
+    { name = "treesitter" },
+    { name = "copilot", group_index = 2 },
+  },
+  cmdline = {
+    enable = true,
+    options = {
+      {
+        type = ":",
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      },
+      {
+        type = { "/", "?" },
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      },
+    },
+  },
+})
---
+++ lua/plugins/cmp/init.lua
@@ -0,0 +1,64 @@
+-- Context aware auto-completion and snippet plugins
+
+return {
+  -- auto-completion
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      -- snippet Engine & its associated nvim-cmp source
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+
+      -- adds a number of user-friendly snippets
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+
+      -- Add Lua GitHub copilot as a completion source
+      "zbirenbaum/copilot-cmp",
+    },
+    config = function()
+      require("plugins.cmp.config")
+    end
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    opts = {},
+    config = function(_, opts)
+      local luasnip = require("luasnip")
+
+      -- vscode format
+      require("luasnip.loaders.from_vscode").lazy_load()
+      -- lua format
+      require("luasnip.loaders.from_lua").load()
+
+      luasnip.config.setup(opts)
+    end
+  },
+
+  -- adds LSP completion capabilities
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    config = function()
+      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    end
+  },
+
+  -- GitHub copilot in Lua
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  }
+}
