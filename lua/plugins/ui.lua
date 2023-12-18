555765e288f95744039f65eacb57270ba599831f
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-18 01:00:42 +0100

    feat: add bufferline
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -78,18 +78,71 @@ return {
     },
   },
 
-  -- TODO nice tab line
+  -- fancy-looking tabs with filetype icons and close buttons
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
+    opts = {
+      options = {
+        mode = "buffers", -- or tabs
+        -- stylua: ignore
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = true,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config").icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File explorer",
+            text_align = "left",
+            highlight = "Directory",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd("BufAdd", {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
 
   -- status line
   {
     "nvim-lualine/lualine.nvim",
-    event = "VimEnter",
+    event = "VeryLazy",
     opts = {
       options = {
+        theme = "auto",
         icons_enabled = true,
-        theme = "catppuccin",
+        globalstatus = true,
         disabled_filetypes = {
           statusline = {
+            "dashboard",
             "NvimTree",
             "help",
             "undotree",
@@ -101,6 +154,12 @@ return {
           }
         },
       },
+      extensions = {
+        "lazy",
+        "mason",
+        "nvim-tree",
+        "trouble",
+      }
     },
   },
 
