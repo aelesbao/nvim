4c83fb7edfd29ce672c0b9c54c9168c1a7eb9660
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-19 12:55:11 +0100

    core: use vim.opt
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,106 +1,105 @@
-local g = vim.g
-local o = vim.opt
-
-g.mapleader      = " "
-g.maplocalleader = "\\"
+vim.g.mapleader      = " "
+vim.g.maplocalleader = "\\"
 
 -- General
 
+local opt = vim.opt
+
 -- persistent undo
-o.undofile   = true
-o.undolevels = 10000
+opt.undofile   = true
+opt.undolevels = 10000
 
 -- increase commands history
-o.history = 1000
+opt.history = 1000
 
 -- creates a backup file
-o.backup      = false
+opt.backup      = false
 -- if a file is being edited by another program, or was written to file while
 -- editing with another program, it is not allowed to be edited
-o.writebackup = false
+opt.writebackup = false
 
 -- interval for writing swap file to disk, also used by gitsigns
-o.updatetime = 250
+opt.updatetime = 250
 
-o.autowrite = true -- enable auto write
-o.autoread  = true -- read files modified outside of vim
+opt.autowrite = true -- enable auto write
+opt.autoread  = true -- read files modified outside of vim
 
 -- required to keep multiple buffers and open multiple buffers
-o.hidden = true
+opt.hidden = true
 
 -- allows neovim to access the system clipboard 
-o.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 
 -- sends more characters to terminal, improving window redraw
-o.ttyfast = true
+opt.ttyfast = true
 
 -- Global timeout settings (also affects which-key response time)
-o.timeout    = true
-o.timeoutlen = 300
+opt.timeout    = true
+opt.timeoutlen = 300
 
 -- Formatting
 
 -- indentation
-o.tabstop     = 2
-o.softtabstop = 2
-o.shiftwidth  = 2
-o.shiftround  = true  -- round indent
-o.expandtab   = true  -- use spaces instead of tabs
-o.smarttab    = true  -- smarter tab levels
-o.smartindent = true  -- smart (language based) auto indent
-o.autoindent  = true  -- copy indent from current line when starting a new line
+opt.tabstop     = 2
+opt.softtabstop = 2
+opt.shiftwidth  = 2
+opt.shiftround  = true  -- round indent
+opt.expandtab   = true  -- use spaces instead of tabs
+opt.smarttab    = true  -- smarter tab levels
+opt.smartindent = true  -- smart (language based) auto indent
+opt.autoindent  = true  -- copy indent from current line when starting a new line
 
 -- text wrapping
-o.linebreak = true  -- don't break wrapped lines on words
-o.textwidth = 0     -- don't wrap lines by default
-o.wrap      = false -- disable line wrap
+opt.linebreak = true  -- don't break wrapped lines on words
+opt.textwidth = 0     -- don't wrap lines by default
+opt.wrap      = false -- disable line wrap
 
 -- file type detection
-o.filetype        = "on" -- auto detect the type of file that is being edited
-o.filetype.indent = "on" -- enable file type detection
-o.filetype.plugin = "on" -- enable filetype-based indentation
+opt.filetype        = "on" -- auto detect the type of file that is being edited
+opt.filetype.indent = "on" -- enable file type detection
+opt.filetype.plugin = "on" -- enable filetype-based indentation
 
 -- sets the default file encoding
-o.fileencoding = "utf-8"
+opt.fileencoding = "utf-8"
 
 -- Visual
 
 -- enable 24 bit terminal colors
-o.termguicolors = true
+opt.termguicolors = true
 
 -- tells Nvim what the inherited Terminal colors are
-o.background = "dark"
+opt.background = "dark"
 
-o.ruler       = true -- show ruler
-o.number      = true -- show line numbers
-o.cursorline  = true -- highlights the current line
-o.laststatus  = 3    -- always display the status line
-o.colorcolumn = "80"
+opt.ruler       = true -- show ruler
+opt.number      = true -- show line numbers
+opt.cursorline  = true -- highlights the current line
+opt.laststatus  = 3    -- always display the status line
+opt.colorcolumn = "80"
 
 -- always show the signcolumn, otherwise it would shift the text each time
-o.signcolumn  = "yes"
+opt.signcolumn  = "yes"
 
-o.showmatch = true -- show matching brackets
-o.matchtime = 5    -- bracket blinking
+opt.showmatch = true -- show matching brackets
+opt.matchtime = 5    -- bracket blinking
 
 -- display an incomplete command in the lower right corner of the Vim window
-o.showcmd   = true
+opt.showcmd   = true
 -- shortens messages
 --o.shortmess = "atI"
 -- more space in the neovim command line for displaying messages
-o.cmdheight = 1
+opt.cmdheight = 1
 -- enhanced command line completion
-o.wildmenu = true
+opt.wildmenu = true
 -- at command line, complete longest common string, then list alternatives
-o.wildmode = "longest,list"
+opt.wildmode = "longest,list"
 
 -- set completeopt to have a better completion experience
-o.completeopt = "menuone,noselect"
+opt.completeopt = "menuone,noselect"
 
 -- display unprintable characters by default
-o.list      = true
+opt.list      = true
 -- use the same symbols as TextMate for tabstops and EOLs
-o.listchars = {
+opt.listchars = {
   tab = "▸ ",
   eol = " ",
   trail = "·",
@@ -110,30 +109,30 @@ o.listchars = {
 
 -- Mouse
 if vim.fn.has("mouse") then
-  o.mouse      = "a"      -- enable mouse
-  o.mousehide  = true     -- hide mouse after chars typed
-  o.mousemodel = "popup"  -- right mouse button pops up a menu
-  o.scrolloff  = 5        -- always show X lines of context around the cursor
+  opt.mouse      = "a"      -- enable mouse
+  opt.mousehide  = true     -- hide mouse after chars typed
+  opt.mousemodel = "popup"  -- right mouse button pops up a menu
+  opt.scrolloff  = 5        -- always show X lines of context around the cursor
 end
 
-o.pumblend = 10 -- Popup transparency
+opt.pumblend = 10 -- Popup transparency
 
 -- Splits
-o.splitbelow = true     -- put new windows below current
-o.splitright = true     -- put new windows right of current
-o.splitkeep  = "screen" -- keep the cursor on the same screen line
+opt.splitbelow = true     -- put new windows below current
+opt.splitright = true     -- put new windows right of current
+opt.splitkeep  = "screen" -- keep the cursor on the same screen line
 
 -- Match and search
-o.hlsearch   = true      -- highlight search
-o.ignorecase = true      -- do case in sensitive matching with
-o.smartcase  = true      -- be sensitive when there's a capital letter
-o.incsearch  = true      -- highlight matches as you type
-o.inccommand = "nosplit" -- preview incremental substitute
+opt.hlsearch   = true      -- highlight search
+opt.ignorecase = true      -- do case in sensitive matching with
+opt.smartcase  = true      -- be sensitive when there's a capital letter
+opt.incsearch  = true      -- highlight matches as you type
+opt.inccommand = "nosplit" -- preview incremental substitute
 
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 -- Integrations
 
 if vim.fn.executable("rg") then
-  o.grepprg = "rg --vimgrep"
+  opt.grepprg = "rg --vimgrep"
 end
