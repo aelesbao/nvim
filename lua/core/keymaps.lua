d5a10007a4642ded68d88877a00482f2e8c2804b
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-27 01:46:17 +0100

    chore: remove unused keymaps
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,13 +1,13 @@
 local k = vim.keymap
 
 -- too lazy to hold shift
-k.set({'n', 'v'}, ";", ":", { remap = false })
+k.set({ "n", "v" }, ";", ":", { remap = false })
 
 -- Tabs
 k.set("n", "<s-tab>", ":tabprevious<cr>")
-k.set("n", "<tab>",   ":tabnext<cr>")
+k.set("n", "<tab>", ":tabnext<cr>")
 k.set("n", "<leader><tab>q", ":tabclose<cr>", { desc = "Close tab" })
-k.set("n", "<leader><tab>Q", ":tabonly<cr>",  { desc = "Close other tabs" })
+k.set("n", "<leader><tab>Q", ":tabonly<cr>", { desc = "Close other tabs" })
 
 -- smart join/split lines
 k.set("n", "J", "mzJ`z")
@@ -25,8 +25,8 @@ k.set("n", "N", "Nzzzv")
 -- Text manipulation
 k.set("n", "<leader>=", "gqip", { desc = "Hard-wrap paragraphs of text" })
 
-k.set("x",          "<leader>p", [["_dP]], { desc = "Paste and keep the current buffer" })
-k.set({ "n", "v" }, "<leader>d", [["_d]],  { desc = "Delete and keep the current buffer" })
+k.set("x", "<leader>p", [["_dP]], { desc = "Paste and keep the current buffer" })
+k.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete and keep the current buffer" })
 
 -- show/hide hidden chars
 k.set("n", "<F12>", ":set invlist<cr>", { silent = true })
@@ -36,13 +36,6 @@ k.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   desc = "Search and replace the current text"
 })
 
--- Diagnostics
-k.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-k.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-
 -- Buffers
-k.set("n", "<leader>bl", ":e#<cr>",      { desc = "Switch to last used buffer" })
+k.set("n", "<leader>bl", ":e#<cr>", { desc = "Switch to last used buffer" })
 k.set("n", "<leader>bv", ":vsplit#<cr>", { desc = "Open last used buffer in vertical split" })
-
---map <leader>bw :call Wipeout()<cr>       -- delete all hidden buffers
---nmap <silent> <LocalLeader>bd <Plug>Kwbd -- keep window on buffer delete
