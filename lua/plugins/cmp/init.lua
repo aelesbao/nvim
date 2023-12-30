647795eb22ebe86a6698bfacabd3ebfd35ab677e
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-30 20:32:41 +0100

    chore: use copilot on yaml files
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -57,5 +57,11 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-  }
+    opts = {
+      filetypes = {
+        yaml = true,
+        plaintext = false,
+      }
+    },
+  },
 }
