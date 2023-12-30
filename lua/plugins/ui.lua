42d0521d28647406390f84542c45b8214c26d25d
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-12-30 20:29:44 +0100

    feat: add keymaps to gitsigns hunks and change lualine
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,17 @@
 -- Plugins that change / improve the editor's UI
 
+local function diff_source()
+  ---@diagnostic disable-next-line: undefined-field
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
 return {
   -- paints the background to shows color previews
   {
@@ -47,6 +59,48 @@ return {
       yadm = {
         enable = false
       },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gs.next_hunk() end)
+          return "<Ignore>"
+        end, { desc = "Next change", expr = true })
+
+        map("n", "[c", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gs.prev_hunk() end)
+          return "<Ignore>"
+        end, { desc = "Previous change", expr = true })
+
+        -- Actions
+        map("n", "<leader>ghs", gs.stage_hunk, { desc = "Stage hunk" })
+        map("n", "<leader>ghr", gs.reset_hunk, { desc = "Reset hunk" })
+        map("v", "<leader>ghs", function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end,
+          { desc = "Stage hunk" })
+        map("v", "<leader>ghr", function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end,
+          { desc = "Reset hunk" })
+        map("n", "<leader>ghu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+        map("n", "<leader>ghp", gs.preview_hunk, { desc = "Preview hunk" })
+        map("n", "<leader>ghS", gs.stage_buffer, { desc = "Stage buffer" })
+        map("n", "<leader>ghR", gs.reset_buffer, { desc = "Reset buffer" })
+        map("n", "<leader>ghb", function() gs.blame_line { full = true } end, { desc = "Blame line" })
+        map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
+        map("n", "<leader>ghd", gs.diffthis, { desc = "Diff this" })
+        map("n", "<leader>ghD", function() gs.diffthis("~") end, { desc = "Diff with base" })
+        map("n", "<leader>gtd", gs.toggle_deleted, { desc = "Toggle deleted" })
+
+        -- Text object
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+      end,
     },
   },
 
@@ -167,6 +221,19 @@ return {
             "undotree",
           },
         },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        -- lualine_b = {"branch", "diff", "diagnostics"},
+        lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source }, "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = {
+          "encoding",
+          "fileformat",
+          "filetype"
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
       },
       winbar = {
         lualine_c = {
