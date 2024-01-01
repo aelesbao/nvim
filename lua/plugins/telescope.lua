61c04b1fd0a652c1d7c7144fe83655027edb645c
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-01-01 17:58:53 +0100

    feat: smarter behaviour for ctrl+p
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,3 @@
--- Telescope live_grep in git root
 -- Function to find the git root directory based on the current buffer's path
 local function find_git_root()
   -- Use the current buffer's path as the starting point for the git search
@@ -6,40 +5,49 @@ local function find_git_root()
   local current_dir
   local cwd = vim.fn.getcwd()
   -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
+  if current_file == "" then
     current_dir = cwd
   else
     -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
+    current_dir = vim.fn.fnamemodify(current_file, ":h")
   end
 
   -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
   ---@diagnostic disable-next-line: undefined-field
   if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
+    -- print("Not a git repository. Searching on current working directory")
+    return nil
   end
   return git_root
+end
+
+local function telescope_smart_find_files()
+  local telescope = require("telescope.builtin")
+
+  if find_git_root() then
+    telescope.git_files({ show_untracked = true })
+  else
+    telescope.find_files()
+  end
 end
 
 -- Custom live_grep function to search in git root
 local function telescope_live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
-    require('telescope.builtin').live_grep {
+    require("telescope.builtin").live_grep({
       search_dirs = { git_root },
-    }
+    })
   end
 end
 
 local function telescope_live_grep_open_files()
-  require('telescope.builtin').live_grep {
+  require("telescope.builtin").live_grep({
     grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-  }
+    prompt_title = "Live Grep in Open Files",
+  })
 end
-
 
 return {
   -- Fuzzy finder for anything
@@ -49,30 +57,46 @@ return {
     cmd = "Telescope",
     keys = {
       -- find
-      { "<C-p>",      ":Telescope find_files<cr>",                                        desc = "Find files" },
-      { "<leader>ff", ":Telescope find_files<cr>",                                        desc = "Find files" },
-      { "<leader>fo", ":Telescope oldfiles<cr>",                                          desc = "Find in recent files" },
-      { "<leader>fz", ":Telescope current_buffer_fuzzy_find<cr>",                         desc = "Find in current buffer" },
+      { "<C-p>", telescope_smart_find_files, desc = "Find files" },
+      { "<leader>ff", ":Telescope find_files<cr>", desc = "Find files" },
+      {
+        "<leader>fo",
+        ":Telescope oldfiles<cr>",
+        desc = "Find in recent files",
+      },
+      {
+        "<leader>fz",
+        ":Telescope current_buffer_fuzzy_find<cr>",
+        desc = "Find in current buffer",
+      },
       { "<leader>fa", ":Telescope find_files follow=true no_ignore=true hidden=true<cr>", desc = "Find all" },
-      { "<leader>fb", ":Telescope buffers sort_mru=true sort_lastused=true<cr>",          desc = "Find buffers" },
+      { "<leader>fb", ":Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Find buffers" },
+      { "<leader>fg", ":Telescope git_files<cr>", desc = "Find git files" },
 
       -- search
-      { "<leader>sg", ":Telescope live_grep<cr>",                                         desc = "Live grep" },
-      { "<leader>sG", telescope_live_grep_git_root,                                       desc = "Live grep on git root" },
-      { "<leader>s/", telescope_live_grep_open_files,                                     desc = "Live grep on open files" },
-      { "<leader>sh", ":Telescope help_tags<cr>",                                         desc = "Help tags" },
-      { "<leader>sm", ":Telescope marks<cr>",                                             desc = "Jump to mark" },
-      { "<leader>sn", ":Telescope notify<cr>",                                            desc = "Notifications" },
-      { "<leader>sk", ":Telescope keymaps<cr>",                                           desc = "Keymaps" },
+      { "<leader>sg", ":Telescope live_grep<cr>", desc = "Live grep" },
+      {
+        "<leader>sG",
+        telescope_live_grep_git_root,
+        desc = "Live grep on git root",
+      },
+      {
+        "<leader>s/",
+        telescope_live_grep_open_files,
+        desc = "Live grep on open files",
+      },
+      { "<leader>sh", ":Telescope help_tags<cr>", desc = "Help tags" },
+      { "<leader>sm", ":Telescope marks<cr>", desc = "Jump to mark" },
+      { "<leader>sn", ":Telescope notify<cr>", desc = "Notifications" },
+      { "<leader>sk", ":Telescope keymaps<cr>", desc = "Keymaps" },
 
       -- git
-      { "<leader>gf", ":Telescope git_files<cr>",                                         desc = "Git files" },
-      { "<leader>gc", ":Telescope git_commits<cr>",                                       desc = "Git commits" },
-      { "<leader>gs", ":Telescope git_status<cr>",                                        desc = "Git status" },
+      { "<leader>gc", ":Telescope git_commits<cr>", desc = "Git commits" },
+      { "<leader>gs", ":Telescope git_status<cr>", desc = "Git status" },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make", },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       "folke/trouble.nvim",
     },
     opts = function()
@@ -92,22 +116,22 @@ return {
             "--hidden",
             "--glob=!.git/",
           },
-          set_env = { ["COLORTERM"] = "truecolor", },
+          set_env = { ["COLORTERM"] = "truecolor" },
           mappings = {
             i = {
-              ["<C-M-t>"] = trouble.open_with_trouble
+              ["<C-M-t>"] = trouble.open_with_trouble,
             },
             n = {
-              ["<C-M-t>"] = trouble.open_with_trouble
-            }
-          }
+              ["<C-M-t>"] = trouble.open_with_trouble,
+            },
+          },
         },
         extensions = {
           fzf = {
-            fuzzy = true,                   -- false will only do exact matching
+            fuzzy = true, -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true,    -- override the file sorter
-            case_mode = "smart_case",       -- "smart_case" or "ignore_case" or "respect_case"
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- "smart_case" or "ignore_case" or "respect_case"
           },
         },
       }
