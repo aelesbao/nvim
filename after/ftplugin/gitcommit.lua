3a303592baf4658327f175c4da7924c016a9533e
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-01-01 17:57:58 +0100

    feat: configure conventionalcommit
    Signed-off-by: Augusto Elesbão <aelesbao@users.noreply.github.com>

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,2 +1,11 @@
-vim.bo.textwidth   = 72
-vim.wo.colorcolumn = "+O"
+vim.bo.textwidth = 72
+
+local cmp = require("cmp")
+
+cmp.setup.buffer({
+  sources = cmp.config.sources({
+    { name = "conventionalcommits" },
+    { name = "path" },
+    { name = "buffer" },
+  }),
+})
