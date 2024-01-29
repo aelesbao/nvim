981a5d4b3422d55b1d83f4452f56e648420cea99
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-01-29 16:30:30 +0100

    feat: navigation keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,17 +4,19 @@ local k = vim.keymap
 k.set({ "n", "v" }, ";", ":", { remap = false })
 
 -- jump to the beginning and end of word
-k.set("i", "<Esc>b", "<C-o>b")
-k.set("i", "<Esc>f", "<C-o>w")
+k.set("i", "<Esc>b", "<C-Left>")
+k.set("i", "<Esc>f", "<C-Right>")
 -- jump to the beginning and end of line
-k.set("i", "<C-a>", "<Home>")
-k.set("i", "<C-e>", "<End>")
+k.set("i", "<C-a>", "<Home>", { remap = false })
+k.set("i", "<C-e>", "<End>", { remap = false })
 
 -- Tabs
 k.set("n", "<s-tab>", ":tabprevious<cr>")
 k.set("n", "<tab>", ":tabnext<cr>")
 k.set("n", "<leader><tab>q", ":tabclose<cr>", { desc = "Close tab" })
 k.set("n", "<leader><tab>Q", ":tabonly<cr>", { desc = "Close other tabs" })
+k.set("n", "<leader><tab><", ":tabm -1<cr>", { desc = "Move tab prev" })
+k.set("n", "<leader><tab>>", ":tabm +1<cr>", { desc = "Move tab next" })
 
 -- smart join/split lines
 k.set("n", "J", "mzJ`z")
@@ -26,6 +28,8 @@ k.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
 -- centralize the screen on scroll up/down or search next/previous
 k.set("n", "<C-d>", "<C-d>zz")
 k.set("n", "<C-u>", "<C-u>zz")
+k.set("n", "<C-f>", "<C-f>zz")
+k.set("n", "<C-b>", "<C-b>zz")
 k.set("n", "n", "nzzzv")
 k.set("n", "N", "Nzzzv")
 
