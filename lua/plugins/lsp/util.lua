7691795c19321ddce6a847c87bb2367bcf06f4c9
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-01-29 16:32:29 +0100

    feat: change lsp keymaps and formatting params

--- lua/plugins/lsp/util.lua
+++ lua/plugins/lsp/util.lua
@@ -1,9 +1,11 @@
 local M = {}
 
-local timeout_ms = 10000
-
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
+
+local format_timeout_ms = 3000
+local format_group_name = "lsp_autoformat"
+local format_group = augroup(format_group_name, { clear = false })
 
 function M.buf_kset(bufnr, mode, lhs, rhs, desc)
   local opts = { buffer = bufnr, remap = true, desc = desc }
@@ -15,80 +17,79 @@ function M.setup_mappings(bufnr)
 
   local lsp = vim.lsp.buf
   local diagnostic = vim.diagnostic
-  local telescope = require("telescope.builtin")
 
   kset("n", "K", lsp.hover, "Hover")
   kset("n", "gK", lsp.signature_help, "Signature help")
   kset({ "n", "i", "x" }, "<C-k>", lsp.signature_help, "Signature help")
 
-  kset("n", "gD", function() lsp.declaration({ reuse_win = true }) end, "Goto Declaration")
-  kset("n", "gd", function() lsp.definition({ reuse_win = true }) end, "Goto Definition")
-  kset("n", "gr", function() lsp.references({ reuse_win = true }) end, "References")
+  kset("n", "gD", function() lsp.declaration({ reuse_win = true }) end, "Declaration")
+  kset("n", "gd", function() lsp.definition({ reuse_win = true }) end, "Definition")
+  kset("n", "gr", function() lsp.references() end, "References")
   kset("n", "gI", function() lsp.implementation({ reuse_win = true }) end, "Implementation")
-  kset("n", "gy", function() lsp.type_definition({ reuse_win = true }) end, "Goto Type definition")
+  kset("n", "gy", function() lsp.type_definition({ reuse_win = true }) end, "Type definition")
 
-  kset("n", "<leader>sd", function() telescope.lsp_definitions({ reuse_win = true }) end, "Goto Definition")
-  kset("n", "<leader>sr", telescope.lsp_references, "References")
-  kset("n", "<leader>sI", function() telescope.lsp_implementations({ reuse_win = true }) end, "Goto Implementation")
-  kset("n", "<leader>sy", function() telescope.lsp_type_definitions({ reuse_win = true }) end, "Goto Type Definition")
+  kset({ "n" }, "<leader>ca", lsp.code_action, "Code action")
+  kset({ "n", "i" }, "<M-CR>", lsp.code_action, "Code action")
 
-  kset({ "n", "v" }, "<leader>ca", lsp.code_action, "Code action")
-  kset({ "n", "i" }, "<C-CR>", lsp.code_action, "Code action")
+  if vim.lsp.buf.range_code_action then
+    kset("x", "<M-CR>", vim.lsp.buf.range_code_action)
+  else
+    kset("x", "<M-CR>", vim.lsp.buf.code_action)
+  end
+
   kset("n", "<leader>cr", lsp.rename, "Rename")
-  kset("n", "<S-F6>", lsp.rename, "Rename")
-  kset({ "n", "x" }, "<leader>cf", function() lsp.format({ async = false, timeout_ms = 3000 }) end, "Format code")
+  kset("n", "<F18>", lsp.rename, "Rename")
+  kset("i", "<F18>", "<C-o><S-F6>", "Rename")
+
+  kset(
+    { "n", "x" },
+    "<leader>cf",
+    function() lsp.format({ async = false, timeout_ms = format_timeout_ms }) end,
+    "Format code"
+  )
+  kset(
+    { "n", "x" },
+    "<C-M-l>",
+    function() lsp.format({ async = false, timeout_ms = format_timeout_ms }) end,
+    "Format code"
+  )
 
   kset("n", "gl", diagnostic.open_float, "Show diagnostic in a floating window")
-  kset("n", "[d", diagnostic.goto_next, "Next diagnostic")
-  kset("n", "]d", diagnostic.goto_prev, "Previous diagnostic")
+  kset("n", "[d", diagnostic.goto_prev, "Previous diagnostic")
+  kset("n", "]d", diagnostic.goto_next, "Next diagnostic")
+end
+
+local function is_autoformat_enabled()
+  local autoformat = vim.b.enable_autoformat
+  if autoformat == nil then
+    ---@diagnostic disable-next-line: inject-field
+    vim.b.enable_autoformat = true
+  end
+
+  return (autoformat == 1 or autoformat == true)
 end
 
 function M.buffer_autoformat(client, bufnr, opts)
-  client = client or {}
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   opts = opts or {}
 
-  if vim.b.enable_autoformat == nil then
-    ---@diagnostic disable-next-line: inject-field
-    vim.b.enable_autoformat = 1
-  end
+  if not client.server_capabilities.documentFormattingProvider then return end
 
-  local group = "lsp_autoformat"
-  local desc = "Format current buffer"
-
-  if client.name then
-    group = string.format("buffer_autoformat_%s", client.name)
-    desc = string.format("Format buffer with %s", client.name)
-  end
-
-  local format_group = augroup(group, { clear = false })
-  vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
-
-  if client.server_capabilities and not client.server_capabilities.documentFormattingProvider then
-    return
-  end
+  vim.api.nvim_clear_autocmds({ group = format_group_name, buffer = bufnr })
 
   autocmd("BufWritePre", {
     group = format_group,
-    desc = desc,
+    desc = "Format current buffer",
     buffer = bufnr,
     callback = function()
-      local autoformat = vim.b.enable_autoformat
-      local enabled = (autoformat == 1 or autoformat == true)
-      if not enabled then
-        return
+      if is_autoformat_enabled() then
+        vim.lsp.buf.format({
+          async = false,
+          bufnr = bufnr,
+          timeout_ms = opts.timeout_ms or format_timeout_ms,
+          formatting_options = opts.formatting_options,
+        })
       end
-
-      local config = {
-        async = false,
-        id = client.id,
-        name = client.name,
-        bufnr = bufnr,
-        timeout_ms = opts.timeout_ms or timeout_ms,
-        formatting_options = opts.formatting_options,
-      }
-
-      vim.lsp.buf.format(config)
     end
   })
 end
