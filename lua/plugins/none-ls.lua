ec29ff28cd0c69d248753186a2bdeccd541be207
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-01-29 16:41:44 +0100

    chore: re-configure none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,25 +5,23 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.completion.spell.with({
-          filetypes = { "markdown" },
+          filetypes = { "gitcommit", "markdown", },
         }),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.ansiblelint,
-        null_ls.builtins.diagnostics.buf,
-        null_ls.builtins.diagnostics.commitlint,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.code_actions.gitrebase,
-        null_ls.builtins.code_actions.gitsigns.with({
-          config = {
-            filter_actions = function(title)
-              return title:lower():match("blame") == nil -- filter out blame actions
-            end,
-          }
+        null_ls.builtins.formatting.terraform_fmt,
+        null_ls.builtins.code_actions.eslint_d.with({
+          extra_args = { "--no-warn-ignored" },
         }),
+        null_ls.builtins.code_actions.gitrebase,
         null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.diagnostics.ansiblelint,
+        null_ls.builtins.diagnostics.eslint_d.with({
+          extra_args = { "--no-warn-ignored" },
+        }),
+        null_ls.builtins.diagnostics.hadolint,
+        null_ls.builtins.diagnostics.terraform_validate,
+        null_ls.builtins.diagnostics.tfsec,
       },
     })
   end,
