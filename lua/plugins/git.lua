47b5553602afa5311931b3ebe666932a7432ffb0
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-07-01 14:17:15 +0200

    feat(git): re-enable yadm support

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,12 +8,22 @@ return {
   -- hunks in a commit.
   {
     "lewis6991/gitsigns.nvim",
+    dependencies = {
+      {
+        "seanbreckenridge/gitsigns-yadm.nvim",
+        lazy = true,
+      },
+    },
     opts = {
       signcolumn = true,
       trouble = true,
-      yadm = {
-        enable = true,
-      },
+      _on_attach_pre = function(_, callback)
+        if vim.fn.executable("yadm") == 1 then
+          require("gitsigns-yadm").yadm_signs(callback)
+        else
+          callback()
+        end
+      end,
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
