85da7ff0e38f3c720cd0ee471c243ca84417f1aa
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-12-02 16:19:29 +0100

    perf(treesitter): disable current scope highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -103,7 +103,7 @@ return {
           clear_on_cursor_move = true,
         },
         highlight_current_scope = {
-          enable = true,
+          enable = false,
         },
       }
     },
