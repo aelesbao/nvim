4986bbe42222b64f9e0730663e46e4c8ae7ba724
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-05-15 19:50:07 +0200

    refactor(ai): move plugins to directory structure

--- lua/plugins/ai/codecompanion.lua
+++ lua/plugins/ai/codecompanion.lua
@@ -29,34 +29,6 @@ local function extend_ollama(adapter, model, context)
 end
 
 return {
-  -- GitHub copilot in Lua
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      panel = { enabled = false },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-y>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-c>",
-        },
-      },
-      filetypes = {
-        yaml = true,
-        plaintext = false,
-      },
-      copilot_model = "gpt-4o-copilot", -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
-    },
-  },
-
   -- AI-powered coding, seamlessly in Neovim
   {
     "olimorris/codecompanion.nvim",
