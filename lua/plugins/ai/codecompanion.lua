c24959bc2b6a89d7f35309d4bc010e5a0842102e
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-05-19 17:32:58 +0200

    feat(ai): add codecompanion-history plugin

--- lua/plugins/ai/codecompanion.lua
+++ lua/plugins/ai/codecompanion.lua
@@ -36,12 +36,14 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/mcphub.nvim",
+      "ravitemer/codecompanion-history.nvim",
     },
     keys = {
       { "<leader>ac", ":CodeCompanionChat<cr>",    desc = "Toggle CodeCompanion chat buffer" },
       { "<leader>ai", ":CodeCompanion<cr>",        desc = "Use the CodeCompanion Inline Assistant" },
       { "<leader>aa", ":CodeCompanionActions<cr>", desc = "Open the CodeCompanion actions palette" },
       { "<leader>ad", ":CodeCompanionCmd<cr>",     desc = "Prompt the LLM to write a command for the command-line" },
+      { "<leader>ah", ":CodeCompanionHistory<cr>", desc = "Opens CodeCompanion history browser" },
     },
     ---@type CodeCompanion
     opts = {
@@ -133,6 +135,31 @@ return {
             make_slash_commands = true, -- make /slash_commands from MCP server prompts
             show_result_in_chat = true, -- Show the mcp tool result in the chat buffer
           },
+        },
+        history = {
+          enabled = true,
+          opts = {
+            -- Keymap to open history from chat buffer (default: gh)
+            keymap = "gh",
+            -- Keymap to save the current chat manually (when auto_save is disabled)
+            save_chat_keymap = "sc",
+            -- Save all chats by default (disable to save only manually using 'sc')
+            auto_save = true,
+            -- Number of days after which chats are automatically deleted (0 to disable)
+            expiration_days = 0,
+            -- Picker interface ("telescope" or "snacks" or "fzf-lua" or "default")
+            picker = "telescope",
+            -- Automatically generate titles for new chats
+            auto_generate_title = true,
+            ---On exiting and entering neovim, loads the last chat on opening chat
+            continue_last_chat = false,
+            ---When chat is cleared with `gx` delete the chat from history
+            delete_on_clearing_chat = false,
+            ---Directory path to save the chats
+            dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+            ---Enable detailed logging for history extension
+            enable_logging = false,
+          }
         },
       },
     },
