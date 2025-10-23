690fd3f5eb12215174d00c4a07e71899bdca025a
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-10-23 23:12:56 +0200

    feat(lazy): add keymap for quick access

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
   -- allow lazy.nvim to manage itself
-  { "folke/lazy.nvim", tag = "stable" },
+  {
+    "folke/lazy.nvim",
+    tag = "stable",
+    init = function()
+      vim.keymap.set("n", "<leader>l", ":Lazy<cr>", { desc = "Lazy" })
+    end
+  },
 }
